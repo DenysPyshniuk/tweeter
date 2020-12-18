c11286d858656c8b867767df3b9b309138396370
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-18 09:26:45 +0000

    Fix function to show errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,11 @@
+//Function to show a counter of characters and to show and hide errors
 $(document).ready(function() {
-    $("#tweet-text").on("input", function() {
+    $("#tweet-text").on("keyup", function() {
         let length = $(this).val().length;
+        if (length > 0 && length <= 140) {
+            $('#error1').hide();
+            $('#error2').hide();
+        }
         let $counter = $(".counter");
         $counter.val(140 - length);
         if ($counter.val() < 0) {
