90d8619e70686c1fcf24ef69c91ab936c08e4677
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-18 09:25:45 +0000

    Add rest of the functions for functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,82 +1,100 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+//jQuery's document ready function
 $(document).ready(function() {
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const tweetData = [{
-            user: {
-                name: "Cosmo",
-                avatars: "fas fa-user-astronaut fa-3x",
-                handle: "@Armstrong",
-            },
-            content: {
-                text: "Everyting is going okay!",
-            },
-            created_at: 1461116232227,
-        },
 
-        {
-            user: {
-                name: "Pluto",
-                avatars: "fas fa-robot fa-3x ",
-                handle: "@Alian",
-            },
-            content: {
-                text: "How it's going there on The Earth?",
-            },
-            created_at: 1461116232227,
-        },
-    ];
-
-    function createTweetElement(tweet) {
-        const name = tweet.user.name;
-        const avatars = tweet.user.avatars;
-        const handle = tweet.user.handle;
-        const text = tweet.content.text;
-        const timePosted = tweet.created_at;
-        const timePast = moment(timePosted).toNow(true);
-        let $tweet = `<article class="tweet-article">
-              <div class="top-article">
-                <div class="icon-name">
-                  <i class = '${avatars}' > </i>
-                  <p> ${name} </p>
-                </div>
-                <div class="handle">
-                  <p> ${handle} </p>
-                </div>
-              </div>
-              <header class = "header-article">
-                <h5> ${text} </h5>
-              </header>
-              <footer class = "footer-article">
-                <div >
-                  <p> ${timePast} </p>
-                  <i class="fas fa-retweet"></i>
-                </div>
-              </footer>
-            </article> `;
-        return $($tweet);
-    }
-
+//Function takes in array of objects and render them to the DOM
     const renderTweets = function(tweetData) {
         // loops through tweets
         for (let element of tweetData) {
             // calls createTweetElement for each tweet
             const $tweet = createTweetElement(element);
             // takes return value and appends it to the tweets container
-            // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-            $("#tweets-container").append($tweet);
+            $("#tweets-container").prepend($tweet);
         }
     };
 
-    //Preventing Default Behaviour of the Form
+//GET ajax request for tweet
+    const loadTweets = function() {
+        $.ajax({
+            type: "GET",
+            url: "/tweets",
+            dataType: "json",
+            success: function(data) {
+                console.log('Success: ', data);
+                $("#tweets-container").empty();
+                renderTweets(data);
+            }
+        });
+    }
+    loadTweets();
+
+//Validates if input was provided and it's not more then 140 characters
+    const validation = function(tweet) {
+        if (tweet === '') {
+            $('#error1').slideDown();
+        } else if (tweet.length > 140) {
+            $('#error2').slideDown()
+        } else {
+            return true;
+        }
+    }
+
+//Escape function to prevent vulnerability of the textarea
+    const escape = function(str) {
+        let div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    }
+//Function to create new article
+    const createTweetElement = function(tweet) {
+        const escape = function(str) {
+            let div = document.createElement('div');
+            div.appendChild(document.createTextNode(str));
+            return div.innerHTML;
+        }
+        const name = tweet.user.name;
+        const avatars = 'fas fa-user-astronaut fa-3x';
+        const handle = tweet.user.handle;
+        const text = tweet.content.text;
+        const timePosted = tweet.created_at;
+        const timePast = moment(timePosted).toNow(true);
+        let $tweet = `<article class="tweet-article">
+                <header class="top-article">
+                <div class="icon-name">
+                    <i class = '${escape(avatars)}' > </i>
+                    <p> ${escape(name)} </p>
+                </div>
+                <div class="handle">
+                    <p> ${escape(handle)} </p>
+                </div>
+                </header>
+                <body class = "body-article">
+                <h5> ${escape(text)} </h5>
+                </body>
+                <footer class = "footer-article">
+                    <div class="time">
+                    <p> ${escape(timePast)} </p>
+                </div>
+                <div class="share-icons">
+                    <i class="far fa-bookmark"></i>
+                    <i class="far fa-share-square"></i>
+                    <i class="far fa-heart"></i>
+                </div>
+                </footer>
+            </article> `;
+        return $($tweet);
+    }
+//Preventing Default Behaviour of the Form & Post a tweet
     $("form").submit(function(event) {
         event.preventDefault();
-        $.ajax({ type: "POST", url: "/tweets", data: $(this).serialize() })
-            .then(console.log("It works!"));
+        if (validation($("#tweet-text").val())) {
+            validation($("#tweet-text").val());
+            $.ajax({ type: "POST", url: "/tweets", data: $(this).serialize() })
+                .then(() => {
+                    console.log('It works!');
+                    $("#tweet-text").val("");
+                    $(".counter").val("140")
+                    loadTweets();
+                });
+        }
     });
-
-    renderTweets(tweetData);
 });
